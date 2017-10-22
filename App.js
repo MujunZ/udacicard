559fe35a63e2668803d161b87fe14bd609714c83
MujunZ <o.6180339@gmail.com>
2017-10-21 22:29:32 -0400

    add Tabs

--- App.js
+++ App.js
@@ -3,15 +3,31 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
+import { TabNavigator, StackNavigator } from "react-navigation";
 import DeckList from './components/DeckList';
 import AddDeck from './components/AddDeck';
+
+const Tabs = TabNavigator({
+  DeckList: {
+    screen: DeckList,
+    navigationOptions: {
+      tabBarLabel: 'Cards'
+    }
+  },
+  AddDeck: {
+    screen: AddDeck,
+    navigationOptions: {
+      tabBarLabel: 'Add'
+    }
+  }
+})
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={styles.container}>
-          <AddDeck />
+          <Tabs />
         </View>
       </Provider>
     );
@@ -22,7 +38,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
